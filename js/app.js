85036d3d5c5a3ebae0e0fd675cd8bf5c7e30575d
Akhand Pratap Singh <104713627+Chickisnak@users.noreply.github.com>
2022-10-21 19:40:11 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const endDate = "15 June 2023 10:00 PM";
+const inputs = document.querySelectorAll("input");
+
+document.getElementById("end-date").innerHTML = endDate;
+
+const clock = ()=>{
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now)/1000;
+  if(diff<0)return;
+  inputs[0].value = Math.floor(diff/3600/24);
+  inputs[1].value = ((Math.floor(diff/3600))%24);
+  inputs[2].value = ((Math.floor(diff/60))%60);
+  inputs[3].value = (Math.floor(diff)%60);
+}
+
+setInterval(
+  ()=>{
+    clock();
+  },1000
+);
